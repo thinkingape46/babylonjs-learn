8b1b9c41f2a7540607fa07529349051c40d006fc
jagadeesh <jagadeesh.kumar2091@gmail.com>
2022-07-23 03:44:29 +0530

    added new meshes

--- src/scripts/babylon/meshes.js
+++ src/scripts/babylon/meshes.js
@@ -4,19 +4,35 @@ import {
   StandardMaterial,
   Color3,
   Texture,
+  Vector4,
+  Mesh,
+  Vector3,
+  AxesViewer,
+  CreateCylinder,
 } from "@babylonjs/core";
 import "@babylonjs/loaders/glTF";
+import earcut from "earcut";
 
 import createScene from "./scene";
 
 const [engine, scene] = createScene();
 
+// axis
+const axes = new AxesViewer(scene, 1);
+
 const addMeshes = async () => {
   // house base
+  const faceUV = [];
+  faceUV[0] = new Vector4(0.0, 0.0, 0.25, 1);
+  faceUV[1] = new Vector4(0.25, 0.0, 0.5, 1);
+  faceUV[2] = new Vector4(0.5, 0.0, 0.75, 1);
+  faceUV[3] = new Vector4(0.75, 0.0, 1, 1);
   const houseBase = MeshBuilder.CreateBox("houseBase", {
     width: 1,
     height: 1,
     depth: 1,
+    faceUV: faceUV,
+    wrap: true,
   });
   const houseRoof = MeshBuilder.CreateCylinder("houseRoof", {
     diameter: 1.3,
@@ -28,7 +44,7 @@ const addMeshes = async () => {
   await SceneLoader.ImportMeshAsync(
     ["ground", "track", "track-path"],
     "./scenes/",
-    "munnekolala-v.0.5.gltf"
+    "munnekolala-v.0.6.gltf"
   );
 
   const ground = scene.meshes.filter((m) => m.name === "ground")[0];
@@ -49,7 +65,7 @@ const addMeshes = async () => {
   roofMaterial.diffuseTexture = new Texture("./images/roof-texture.jpg", scene);
   const wallMaterial = new StandardMaterial("wallMaterial", scene);
   wallMaterial.diffuseTexture = new Texture(
-    "./images/floor-texture.png",
+    "./images/cubehouse-texture.png",
     scene
   );
 
@@ -59,7 +75,78 @@ const addMeshes = async () => {
   houseRoof.material = roofMaterial;
   houseBase.material = wallMaterial;
 
-  console.log(ground.position.y);
+  // merge meshes
+  const house = Mesh.MergeMeshes(
+    [houseBase, houseRoof],
+    true,
+    false,
+    null,
+    false,
+    true
+  );
+
+  // scaling the house
+  house.position.x = 6;
+  house.position.z = 6;
+  house.scalingDeterminant = 3;
+
+  // building a car
+  const carOutline = [new Vector3(0, 0, 0), new Vector3(2, 0, 0)];
+
+  const carMirrorOutlinePoints = 60;
+  for (let i = 45; i <= carMirrorOutlinePoints; i++) {
+    carOutline.push(
+      new Vector3(
+        2 - Math.sin(Math.PI * (i / 30)),
+        0,
+        Math.cos(Math.PI * ((carMirrorOutlinePoints - i) / 30))
+      )
+    );
+  }
+  carOutline.push(new Vector3(0, 0, Math.cos(0)));
+  carOutline.push(new Vector3(0, 0, 0));
+
+  const car = new MeshBuilder.ExtrudePolygon(
+    "car",
+    {
+      shape: carOutline,
+      depth: 1,
+    },
+    scene,
+    earcut
+  );
+
+  car.scalingDeterminant = 1;
+  house.computeWorldMatrix(true);
+  car.computeWorldMatrix(true);
+
+  // building wheels
+  const wheelLF = new CreateCylinder("wheelLF", {
+    diameter: 0.5,
+    height: 0.1,
+  });
+  wheelLF.parent = car;
+  wheelLF.position.x = 2.5;
+  wheelLF.position.y = -1;
+
+  const wheelRF = wheelLF.clone("wheelRF");
+  wheelRF.position.y = 0;
+
+  const wheelLB = wheelLF.clone("wheelLB");
+  wheelLB.position.x = 1;
+
+  const wheelRB = wheelLF.clone("wheelRB");
+  wheelRB.position.x = 1;
+  wheelRB.position.y = 0;
+
+  car.rotation.x = -Math.PI / 2;
+
+  console.log(135, wheelRF);
+
+  // setting axes
+  axes.xAxis.parent = car;
+  axes.yAxis.parent = car;
+  axes.zAxis.parent = car;
 
   engine.runRenderLoop(() => scene.render());
 };
